aae0b8b207ba099b59366f3187e8d88c2b52144c
Prerona <preronaghosh1997@gmail.com>
2023-06-03 18:32:22 -0400

    Updated limit and exit logic

--- client.py
+++ client.py
@@ -30,41 +30,39 @@ def main():
         print(f"r_port: {r_port}")
         client_tcp_socket.close()
 
-        # Establish UDP connection on r_port for each word in list
-        client_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        for w in words:
-            print("Sending a message", flush=True)
-            client_udp_socket.sendto(w.encode('utf-8'), (server_address, int(r_port)))
-            print("Sent a message to server", flush=True)
+        if r_port != "Incorrect Req Code":
+            # Establish UDP connection on r_port for each word in list
+            client_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            limit_reached = 0
+            for w in words:
+                print("Sending a message", flush=True)
+                client_udp_socket.sendto(w.encode('utf-8'), (server_address, int(r_port)))
+                print("Sent a message to server", flush=True)
 
-            server_msg = client_udp_socket.recvfrom(1024)[0].decode('utf-8')
-            print(f"Server message: {server_msg}", flush=True)
+                server_msg = client_udp_socket.recvfrom(1024)[0].decode('utf-8')
+                print(f"Server message: {server_msg}", flush=True)
 
-            if server_msg == 'TRUE' or server_msg == 'FALSE':
-                results.append(server_msg)
+                if server_msg == 'TRUE' or server_msg == 'FALSE':
+                    results.append(server_msg)
 
-            if server_msg == 'LIMIT':
-                print("UDP Connection to client closed as EXIT was received", flush=True)
-                results.append("Request limit reached")
-                client_udp_socket.close()
-                break
+                if server_msg == 'LIMIT':
+                    limit_reached = 1
+                    print("UDP Connection to client closed as LIMIT was received", flush=True)
+                    results.append("Request limit reached")
+                    break
 
-            if w == words[len(words) - 1]:  # reached the last string
-                print("Sending the last word", flush=True)
+            if limit_reached == 0:
+                print("Sent EXIT to server")
                 client_udp_socket.sendto("EXIT".encode('utf-8'), (server_address, int(r_port)))
-                client_udp_socket.close()
-                break
 
-            # print("Going to send the next message to server", flush=True)
+            client_udp_socket.close()
 
         # Print the final results array
+        final_res = ""
         for res in results:
-            if res != results[-1]:
-                print(res + ', ', end='')
-            else:
-                print(res, end='')
-
-        client_udp_socket.close()
+            final_res += res + ", "
+        final_res = final_res[:-2]  # remove comma and space
+        print(final_res)
 
 
 if __name__ == '__main__':
